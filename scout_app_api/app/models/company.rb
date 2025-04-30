0140d55e2043066818cfb18ecb3525e913a0b05d
hayaf3254 <poso42731427@gmail.com>
2025-05-01 02:47:20 +0900

    企業の登録ページと機能完了,これにて学生と企業のログインと登録と機能、ページが完了（CSS適応なし）
    !# Please enter the commit message for your changes. Lines starting

--- scout_app_api/app/models/company.rb
+++ scout_app_api/app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ApplicationRecord
+    validates :user_id, presence: true, uniqueness: { message: "は既に使用されています。他のIDを使用してください。" }
+    validates :name, presence: true
+    validates :password, presence: true
+    validates :publish, presence: true
 end
