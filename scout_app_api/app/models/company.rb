77885a3f9f3c8aff110d75139f0bd2362f0c6011
hayaf3254 <poso42731427@gmail.com>
2025-05-09 21:34:26 +0900

    バックエンド側にある/models/company.rbにコメントアウトを追加した

--- scout_app_api/app/models/company.rb
+++ scout_app_api/app/models/company.rb
@@ -1,4 +1,7 @@
 class Company < ApplicationRecord
+    #validatesで各カラムに対するバリデーション（入力ルール）を設定
+    #presence: trueは必須項目であることを示す
+    #uniqueness → 他のレコードと重複不可、messageでエラー文を変更できる
     validates :user_id, presence: true, uniqueness: { message: "は既に使用されています。他のIDを使用してください。" }
     validates :name, presence: true
     validates :password, presence: true
