fdad0dacbefb8392bc408765ec00d3c287546963
hayaf3254 <poso42731427@gmail.com>
2025-05-23 01:39:50 +0900

    Fix ESLint link issues

--- scout-front/src/pages/company/companies_register_success.js
+++ scout-front/src/pages/company/companies_register_success.js
@@ -1,11 +1,14 @@
 import styles from '@/styles/app.module.css';
+import Link from 'next/link';
 
 export default function CompanyRegisterSuccess() {
     return (
       <div className={styles.container}>
         <h1 className={styles.title}>登録完了</h1>
         <p className={styles.message}>企業アカウントが正常に登録されました！</p>
-        <a href="/company/company_login" className={styles.link}>ログインページへ</a>
+        <Link href="/company/company_login/">
+          ログイン画面へ
+        </Link>
       </div>
     );
   }
--- scout-front/src/pages/student/student_register_success.js
+++ scout-front/src/pages/student/student_register_success.js
@@ -1,11 +1,14 @@
 import styles from '@/styles/app.module.css';
+import Link from 'next/link';
 
 export default function StudentRegisterSuccess() {
     return (
       <div className={styles.container}>
         <h1 className={styles.title}>登録完了</h1>
         <p className={styles.message}>学生アカウントが正常に登録されました！</p>
-        <a href="/student/student_login" className={styles.link}>ログインページへ</a>
+        <Link href="/student/student_login/">
+          ログイン画面へ
+        </Link>
       </div>
     );
   }
